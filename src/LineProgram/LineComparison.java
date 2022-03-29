9c142e487f7ff3ba18e0beab4d10a54e71e4af03
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-29 17:11:26 +0530

    added uc1.2

---
+++ src/LineProgram/LineComparison.java
@@ -0,0 +1,21 @@
+package LineProgram;
+
+public class LineComparison {
+	public static void main(String[]args) {
+	Line line=new Line();
+	line.setX1(12);
+	line.setX2(1);
+	line.setY1(10);
+	line.setY2(14);
+	Line line2=new Line();
+	line2.setX1(5);
+	line2.setX2(4);
+	line2.setY1(4);
+	line2.setY2(7);
+	System.out.println(line.toString());
+	System.out.println(line2.toString());
+
+
+
+	}
+}
